2a9c9d7ab72e027516c904352198ab0187e87428
TonySu479 <tonysu479@gmail.com>
2021-02-04 13:30:01 +0100

    Signup Activity

---
+++ app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -0,0 +1,29 @@
+package com.example.projemanag
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_intro.*
+import kotlinx.android.synthetic.main.activity_splash.tv_app_name
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        tv_app_name.typeface = typeFace
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+    }
+
+
+}
