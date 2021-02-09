ad555d1289a41af65bc5f0f71a1b5089b79d934a
TonySu479 <tonysu479@gmail.com>
2021-02-09 20:47:54 +0100

    TaskList Title Retrieve

--- app/src/main/java/com/example/projemanag/firebase/FireStoreClass.kt
+++ app/src/main/java/com/example/projemanag/firebase/FireStoreClass.kt
@@ -25,6 +25,21 @@ class FirestoreClass {
             }
     }
 
+    fun getBoardDetails(activity: TaskListActivity, documentId: String){
+        mFireStore.collection(Constants.BOARDS)
+            .document(documentId)
+            .get()
+            .addOnSuccessListener {
+                    document ->
+                Log.i(activity.javaClass.simpleName, document.toString())
+                val boardList: ArrayList<Board> = ArrayList()
+                activity.boardDetails(document.toObject(Board::class.java)!!)
+            }.addOnFailureListener { e ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, "Error while creating boards List", e)
+            }
+    }
+
     fun createBoard(activity: CreateBoardActivity, board: Board){
         mFireStore.collection(Constants.BOARDS)
             .document()
@@ -131,4 +146,6 @@ class FirestoreClass {
         }
         return currentUserId
     }
+
+
 }
